5f7f6d7c9ad3b4cef2c153312b0a868806d23817
Townsy <townsy@revobot.xyz>
2021-02-20 03:45:19 +0000

    :sparkles: Updated to start adding in options and the message listener to handle commands.

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A simple Discord bot command handler that is easy to use, built for Discord.js
 ## Usage
 
 ```bash
-Do something here
+Do something here this shit crazy
 ```
--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('Big package for you!');
+require('dotenv').config();
+const Discord = require('discord.js');
+const Log = require('mustang-log');
+
+/**
+ * Core function
+ * @param client - The client instance.
+ * @param options - Optional params to customise the bot.
+ */
+module.exports = async (client, options) => {
+  if (!client) throw new Error('Client must be provided to continue');
+  const {
+    commandsDir = './commands',
+    eventsDir = './events',
+    allowDMs = true
+  } = options;
+
+  client.commands = await registerCommands(commandsDir);
+  client.events = await registerEvents(eventsDir);
+
+  client.on('message', (message) => {
+    if (message.bot || (!allowDMs && !message.guild)) return null;
+  });
+}
+
+(async () => {
+  console.log(await module.exports('client', { eventsDir: 'myDir' }))
+})();
+
+
+
+async function registerCommands(dir) {
+  if (!dir) return [];
+
+  // TODO - Add command handler from my bot
+}
+
+async function registerEvents(dir) {
+  if (!dir) return [];
+
+  // TODO - Add events from my bot
+}
